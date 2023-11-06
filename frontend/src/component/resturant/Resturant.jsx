019215d13a6b56b1b1e7334aba04a7c0e1e5e4e5
Shashank Naik <shashanknaik808@gmail.com>
2023-11-06 12:05:43 +0530

    Modifying Restaurant.js

--- frontend/src/component/resturant/Resturant.jsx
+++ frontend/src/component/resturant/Resturant.jsx
@@ -3,7 +3,17 @@ import './Resturant.css';
 
 function Resturant(props) {
 
-    
+
+    async function getData() {
+        let backend_url = 'http://localhost:3200/data';
+        let response = await fetch(backend_url);
+
+        console.log(response);
+        let responseData = await response.json();
+
+        console.log(responseData);
+    }
+
 
     return (
         <div>
