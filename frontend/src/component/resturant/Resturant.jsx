a9d4436a860a679f062f78ca6b5bea69ad7673d3
Shashank Naik <shashanknaik808@gmail.com>
2023-11-06 13:40:19 +0530

    Using Card Component of Bootstrap for Food Container

--- frontend/src/component/resturant/Resturant.jsx
+++ frontend/src/component/resturant/Resturant.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import './Resturant.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function Resturant(props) {
 
@@ -31,14 +33,29 @@ function Resturant(props) {
         <div className='pri'>
             <div>
                 <h1>Food Menu</h1>
-                {
-                    menuData.flag ?
-                        menuData.foodData.map(item => {
-                            return <li key={item.foodName}>
-                                Food Name is {item.foodName}, Price: {item.price}, Catogary: {item.catogory}
-                            </li>
-                        }) : <h1>Loading</h1>
-                }
+                <div className='food-container'>
+                    {
+                        menuData.flag ?
+                            menuData.foodData.map(item => {
+                                return <Card className='food-item' style={{ width: '18rem' }} key={item.foodName}>
+                                    <Card.Img variant="top" src={'images/food/' + item.foodName + '.jpg'} />
+                                    <Card.Body className={(item.catogory == 'veg') ? 'veg' : 'non-veg'}>
+                                        <Card.Title>{item.foodName} : {item.price} </Card.Title>
+                                        <Card.Text>
+                                            {item.foodName} is an amazing Dish. You should try it.
+                                        </Card.Text>
+                                        <Card.Text>
+                                            Catogory: <img src={(item.catogory == 'veg') ? 'images/veg.png' : 'images/non-veg.png'}
+                                                height="30px" width='30px' />
+                                        </Card.Text>
+                                        <Button varient="primary">Buy Now</Button>
+                                    </Card.Body>
+                                </Card>
+                            })
+                            : (<h2>Loading</h2>)
+                    }
+                </div>
+
 
                 <h1>Drink Menu</h1>
                 {
@@ -50,8 +67,8 @@ function Resturant(props) {
                         }) : <h1>Loading</h1>
                 }
 
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
 
